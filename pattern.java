80d19699f04e4b89294b31c98ebf999ebccb7746
Laya <klaya_cse240592@mgit.ac.in>
2026-09-07 15:30:26 +0530

    initial commit of java file

---
+++ pattern.java
@@ -0,0 +1,14 @@
+class pattern
+{
+public static void main(String args[])
+{    
+    for(int i=65;i<=69;i++)
+    {
+        for(int j=65;j<=i;j++)
+    {
+      printf("%c",i);
+    
+    }
+    printf("\n");
+    }
+}
